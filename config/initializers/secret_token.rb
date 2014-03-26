65afff6bc7774cb418a6b0f1ed0f8bbf20dd2a30
Captain Proton <captainproton@captprotonlocal.local>
2014-03-25 19:48:48 -0700

    iniitial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SwimmersNotebook::Application.config.secret_key_base = '56715f86bc46d973aa0fdc46037abf6b7a5bbd4490d4d1cf89fefe60047dda53dfeec7bc896d440883d0859491656cd3bef1267856814d44f6cab39989e80095'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SwimmersNotebook::Application.config.session_store :cookie_store, key: '_swimmers_notebook_session'
